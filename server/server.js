36a78b8913e3dc4d05523789ff32b8b95559a501
Joey Huntington <jwhunt19@gmail.com>
2021-06-15 18:34:17 -0400

    Simplify API call with optional axios param

--- server/server.js
+++ server/server.js
@@ -9,26 +9,15 @@ app.use(express.static(path.join(__dirname, '..', 'dist')));
 
 app.get('/api/:query/:sort', (req, res) => {
   // todo - limit requests to not break github api
-  if (req.params.sort === 'default') {
-    axios.get('https://api.github.com/search/repositories', {
-      params: {
-        q: req.params.query,
-        per_page: 100,
-      },
-    })
-      .then(({ data }) => res.status(200).send(data))
-      .catch((err) => res.send(err));
-  } else {
-    axios.get('https://api.github.com/search/repositories', {
-      params: {
-        q: req.params.query,
-        per_page: 100,
-        sort: req.params.sort,
-      },
-    })
-      .then(({ data }) => res.status(200).send(data))
-      .catch((err) => res.send(err));
-  }
+  axios.get('https://api.github.com/search/repositories', {
+    params: {
+      q: req.params.query,
+      per_page: 100,
+      ...(req.params.sort !== 'default' ? { sort: req.params.sort } : {}),
+    },
+  })
+    .then(({ data }) => res.status(200).send(data))
+    .catch((err) => res.send(err));
 });
 
 module.exports = app;
