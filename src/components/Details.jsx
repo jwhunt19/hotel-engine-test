d1cafc2f4a142a0d44fe377e808bac417e00b329
Joey Huntington <jwhunt19@gmail.com>
2021-06-16 15:09:23 -0400

    Add details page information and formatting

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,12 +1,45 @@
 import React from 'react';
 import {
-  Button, Grid,
-  GridItem, Link, Typography,
+  Button,
+  Grid,
+  Link,
+  Typography,
+  Paper,
+  Avatar,
 } from '@material-ui/core';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import CodeIcon from '@material-ui/icons/Code';
+import DescriptionIcon from '@material-ui/icons/Description';
+import StarOutlineIcon from '@material-ui/icons/StarOutline';
+import CallSplitIcon from '@material-ui/icons/CallSplit';
+import StorageIcon from '@material-ui/icons/Storage';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import { makeStyles } from '@material-ui/core/styles';
 import { useLocation } from 'react-router-dom';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    maxWidth: '900px',
+    margin: 'auto',
+  },
+  gridItem: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+  },
+  gridInfo: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  secondary: {
+    color: theme.palette.text.secondary,
+  },
+}));
+
 const Details = () => {
+  const classes = useStyles();
   const location = useLocation();
   const { repo } = location.state;
 
@@ -15,20 +48,99 @@ const Details = () => {
   };
 
   return (
-    <Grid container alignItems="center">
-      <Button variant="outlined" color="primary" onClick={goBack}>
-        <ArrowBackIcon />
-      </Button>
-      <Typography style={{ marginLeft: '5px' }}>
-        <Link rel="noopener noreferrer" href={repo.owner.html_url} target="_blank">
-          {repo.owner.login}
-        </Link>
-        {' / '}
-        <Link rel="noopener noreferrer" href={repo.html_url} target="_blank">
-          {repo.name}
-        </Link>
-      </Typography>
-    </Grid>
+    <>
+      <Grid container alignItems="center" spacing={3}>
+        <Grid item>
+          <Button variant="outlined" color="primary" onClick={goBack}>
+            <ArrowBackIcon />
+          </Button>
+        </Grid>
+        <Grid item>
+          <Typography>
+            <Link rel="noopener noreferrer" href={repo.owner.html_url} target="_blank">
+              {repo.owner.login}
+            </Link>
+            {' / '}
+            <Link rel="noopener noreferrer" href={repo.html_url} target="_blank">
+              {repo.name}
+            </Link>
+          </Typography>
+        </Grid>
+      </Grid>
+
+      <div className={classes.root}>
+        <Grid container spacing={5}>
+          <Grid item xs={12}>
+            <Typography variant="h2" align="center">{repo.name}</Typography>
+            <Grid item className={classes.gridInfo}>
+              <Typography variant="body2" align="center" className={classes.secondary}>Created by:&nbsp;</Typography>
+              <Avatar alt={`${repo.owner.login} avatar`} src={repo.owner.avatar_url} />
+              <Typography variant="h5" align="center">
+                &nbsp;
+                {repo.owner.login}
+              </Typography>
+            </Grid>
+          </Grid>
+
+          <Grid item xs={12} sm={6} className={`${classes.gridItem} ${classes.gridInfo}`}>
+            <CodeIcon className={classes.secondary} />
+            <Typography variant="body2" className={classes.secondary}>Language:&nbsp;</Typography>
+            <Typography variant="body1">{repo.language}</Typography>
+          </Grid>
+
+          <Grid item xs={12} sm={6} className={classes.gridItem}>
+            <Grid item className={classes.gridInfo}>
+              <DescriptionIcon className={classes.secondary} />
+              <Typography variant="body2" className={classes.secondary}>Description:&nbsp;</Typography>
+            </Grid>
+            <Typography variant="body1">{repo.description}</Typography>
+          </Grid>
+
+          <Grid item xs={6} sm={3}>
+            <Paper className={`${classes.gridItem} ${classes.gridInfo}`}>
+              <StarOutlineIcon className={classes.secondary} />
+              <Typography variant="body2" className={classes.secondary}>Stars:&nbsp;</Typography>
+              <Typography variant="body1">{repo.stargazers_count}</Typography>
+            </Paper>
+          </Grid>
+
+          <Grid item xs={6} sm={3}>
+            <Paper className={`${classes.gridItem} ${classes.gridInfo}`}>
+              <CallSplitIcon className={classes.secondary} />
+              <Typography variant="body2" className={classes.secondary}>Forks:&nbsp;</Typography>
+              <Typography variant="body1">{repo.forks}</Typography>
+            </Paper>
+          </Grid>
+
+          <Grid item xs={6} sm={3}>
+            <Paper className={`${classes.gridItem} ${classes.gridInfo}`}>
+              <StorageIcon className={classes.secondary} />
+              <Typography variant="body2" className={classes.secondary}>Size:&nbsp;</Typography>
+              <Typography variant="body1">
+                {repo.size}
+                {' '}
+                KB
+              </Typography>
+            </Paper>
+          </Grid>
+
+          <Grid item xs={6} sm={3}>
+            <Paper className={`${classes.gridItem} ${classes.gridInfo}`}>
+              <VisibilityIcon className={classes.secondary} />
+              <Typography variant="body2" className={classes.secondary}>Watchers:&nbsp;</Typography>
+              <Typography variant="body1">{repo.watchers}</Typography>
+            </Paper>
+          </Grid>
+
+          <Grid item xs={12} align="center">
+            <Button rel="noopener noreferrer" href={repo.html_url} target="_blank" variant="outlined" className={classes.secondary}>
+              Visit repository
+              <ArrowForwardIcon />
+            </Button>
+          </Grid>
+        </Grid>
+      </div>
+    </>
   );
 };
 export default Details;
