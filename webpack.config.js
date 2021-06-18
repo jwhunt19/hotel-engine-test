bc8463b56065272105173ddb389c33fcb91676cc
Joey Huntington <jwhunt19@gmail.com>
2021-06-18 16:44:47 -0400

    Change mode to production

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: path.resolve(__dirname, './src/index.jsx'),
-  mode: 'development',
+  mode: 'production',
   module: {
     rules: [
       {
